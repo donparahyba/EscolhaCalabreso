221f169e12c9bb8f64f1d28457a799f8bb415e0d
donparahyba <andreihudson@proton.me>
2024-06-07 14:52:31 -0300

    Update resposta.js

--- resposta.js
+++ resposta.js
@@ -4,7 +4,10 @@ function escolher() {
 
     var aleatorio = Math.floor(Math.random() * 2) + 1;
 
-    if(aleatorio == 1){
+    if (opcao1 === '' || opcao2 === '') {
+        document.getElementById('resultado').innerHTML = '<div class="alert alert-danger" role="alert">Preencha os dois!!!</div>';
+    }
+    else if(aleatorio == 1){
         document.getElementById('resultado').innerHTML = "<h2 class=\"text-center\">Calabreso escolheu:</h2><br><h4 class=\"text-center fade-in\">" + opcao1 + "</h4>";
     }
     else{
