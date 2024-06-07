e846c42a529bd2b9e38741a49ec59d154d841427
donparahyba <andreihudson@proton.me>
2024-06-06 22:28:12 -0300

    Update resposta.js

--- resposta.js
+++ resposta.js
@@ -5,9 +5,9 @@ function escolher() {
     var aleatorio = Math.floor(Math.random() * 2) + 1;
 
     if(aleatorio == 1){
-        document.getElementById('resultado').innerHTML = "<h2 class=\"text-center\">Calabreso escolheu:</h2><br><h4 class=\"text-center\">" + opcao1 + "</h4>";
+        document.getElementById('resultado').innerHTML = "<h2 class=\"text-center\">Calabreso escolheu:</h2><br><h4 class=\"text-center fade-in\">" + opcao1 + "</h4>";
     }
     else{
-        document.getElementById('resultado').innerHTML = "<h2 class=\"text-center\">Calabreso escolheu:</h2><br><h4 class=\"text-center\">" + opcao2 + "</h4>";
+        document.getElementById('resultado').innerHTML = "<h2 class=\"text-center\">Calabreso escolheu:</h2><br><h4 class=\"text-center fade-in\">" + opcao2 + "</h4>";
     }
 }
